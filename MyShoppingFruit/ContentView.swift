d328214e30d57c245c080dc56149abbd847a40f4
berkahhariini999-rgb <berkahhariini999@gmail.com>
2025-10-07 09:33:20 +0700

    Initial Commit

---
+++ MyShoppingFruit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyShoppingFruit
+//
+//  Created by Iqbal Alhadad on 07/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyShoppingFruit/MyShoppingFruitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyShoppingFruitApp.swift
+//  MyShoppingFruit
+//
+//  Created by Iqbal Alhadad on 07/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MyShoppingFruitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
